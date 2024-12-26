f6d0c4022525124c45d9adbfeb5bec1a74d5f220
odocoil <wonderduck@protonmail.com>
2024-12-25 20:17:06 -0600

    why artifact. sadge.

--- poke.py
+++ poke.py
@@ -5,7 +5,7 @@ from numpy import linspace, ceil
 from sys import argv
 import shapely
 
-UNI_TOL = 1e-4
+UNI_TOL = 1e-7
 
 class LineSegment:
     point1 = None
@@ -35,29 +35,31 @@ if __name__ == "__main__":
         z_points.append(i*your_mesh.bounding_box.bounds.max(axis=0))
 
     for j in range(len(midpoints)):
-        j = int(ceil((len(midpoints)-1)/2))
+        # j = int(ceil((len(midpoints)-1)/2))
         muh_lines = intersections.mesh_plane(your_mesh, (0,0,1), midpoints[j])
         seg_list = list(map(lambda line : LineSegment(shapely.Point(line[0,0:2]), shapely.Point(line[1,0:2])), muh_lines))
         if len(seg_list) <= 0:
             continue
         poly_segs = [seg_list.pop(0)]
+        END_OF_LOOP = False
         while True:
             current_seg = poly_segs[-1]
             for k in range(len(seg_list)):
                 if seg_list[k].connected(current_seg):
                     poly_segs.append(seg_list.pop(k))
                     break
-            break
+            else:
+                break
+
         poly_points = []
         for s in poly_segs:
             if not is_in(s.point1, poly_points):
-                poly_points.append(shapely.Point(s.point1))
+                poly_points.append(s.point1)
             if not is_in(s.point2, poly_points):
-                poly_points.append(shapely.Point(s.point2))
+                poly_points.append(s.point2)
 
-        print(poly_points)
         my_gon = shapely.Polygon(poly_points)
         x,y = my_gon.exterior.xy
         plt.plot(x,y)
-        break
+        
     plt.show()
